34fb391c3cf27606e3b4d5c2c1b1dd7677883b42
BgmLover <787782453@qq.com>
2018-06-08 18:47:10 +0800

    python 真有趣

--- middleend/block.py
+++ middleend/block.py
@@ -1,11 +1,18 @@
+def count():
+  print('test\n')
+
+
 class Block:
   function_node=None
-  variable_map={}
-  label_map={}
+  variable_map=None
+  label_map=None
   #arrayMap={}
   break_label=None     #如果break，将会跳到哪个label
   continue_label=None  #如果continue，将会跳到哪个label
   goto_label=None      #如果goto，将会跳到哪个label
 
   def __init__(self):
-    pass
+    self.variable_map={}
+    self.label_map={}
+
+
